2f3d272b00164f20e9816350b0530f56606ab1ed
Mukundan Senthil <mukundan314@gmail.com>
2020-01-09 12:21:11 +0530

    Add app

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+import urllib.request
+import urllib.error
+import json
+import datetime
+from weasyprint import HTML
+from flask import Flask, Response, render_template
+
+API_KEY = os.environ.get('API_KEY', 'DEMO_KEY')
+API_URL = "https://api.nasa.gov/planetary/apod?date={}&api_key=" + API_KEY
+
+app = Flask(__name__)
+
+
+def apod_api(date):
+    with urllib.request.urlopen(API_URL.format(date)) as res:
+        return json.load(res)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<date>')
+def apod_html(date):
+    try:
+        start = datetime.date.fromisoformat('1995-05-16')
+        today = datetime.date.today()
+
+        if start <= datetime.date.fromisoformat(date) <= today:
+            data = apod_api(date)
+            return render_template('apod.html',
+                                   date=date,
+                                   title=data['title'],
+                                   explanation=data['explanation'],
+                                   image=data['url'])
+        else:
+            return render_template('404.html')
+    except (ValueError, urllib.error.HTTPError):
+        return render_template('500.html')
+
+
+@app.route('/<date>/pdf')
+def apod_pdf(date):
+    return Response(HTML(string=apod_html(date)).write_pdf(), mimetype="application/pdf")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
